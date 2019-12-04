8e5422c0d376421d5c3867728545a1d33d389a32
Christopher Menedes <cmenedes@yahoo.com>
2019-12-04 12:05:57 -0500

    Update schema.sql
    modify directory for schema sql files

--- src/main/sql/definition/postgresql/schema.sql
+++ src/main/sql/definition/postgresql/schema.sql
@@ -1,13 +1,13 @@
 set role stratum;
 select 'calling stratum schema.sql with user ' || current_user || ' on database ' || current_catalog || ' at ' || version(); 
 set SEARCH_PATH=stratum_catalog,public;
-\i ./src/main/sql/definition/postgresql/schema-stratum_catalog.sql
+\i ../stratum/src/main/sql/definition/postgresql/schema-stratum_catalog.sql
 set SEARCH_PATH=bldg_blue,public;
-\i ./src/main/sql/definition/postgresql/schema-bldg.sql
+\i ../stratum/src/main/sql/definition/postgresql/schema-bldg.sql
 set SEARCH_PATH=bldg_green,public;
-\i ./src/main/sql/definition/postgresql/schema-bldg.sql
+\i ../stratum/src/main/sql/definition/postgresql/schema-bldg.sql
 set SEARCH_PATH=bldg_read,public;
-\i ./src/main/sql/definition/postgresql/schema-bldg_read.sql
+\i ../stratum/src/main/sql/definition/postgresql/schema-bldg_read.sql
 -- taxmap
 -- cscl
 -- basemap
